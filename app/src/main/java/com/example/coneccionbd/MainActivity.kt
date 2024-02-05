8419d4a11ba618ae89f06de352ad7c574eb5b81d
angelasofia9 <ascampos8@misena.edu.co>
2024-02-05 17:39:38 -0500

    22

--- app/src/main/java/com/example/coneccionbd/MainActivity.kt
+++ app/src/main/java/com/example/coneccionbd/MainActivity.kt
@@ -4,15 +4,19 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Toast
 import com.example.coneccionbd.Modem.BdHelper
+import com.example.coneccionbd.Modem.ManangerDb
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        val bdHelper = BdHelper(this)
-        val db = bdHelper.writableDatabase// abro mi bd en mode escritura
-        Toast.makeText(this, "base de datos creada", Toast.LENGTH_SHORT).show()
-        db.close()//cierro la base de datos
+        //val bdHelper = BdHelper(this)
+       // val db = bdHelper.writableDatabase// abro mi bd en mode escritura
+        //Toast.makeText(this, "base de datos creada", Toast.LENGTH_SHORT).show()
+      //  db.close()//cierro la base de datos
+
+        val manager = ManangerDb(this)
+        manager.inserData()
     }
 }
--- app/src/main/java/com/example/coneccionbd/Modem/ManangerDb.kt
+++ app/src/main/java/com/example/coneccionbd/Modem/ManangerDb.kt
@@ -1,4 +1,42 @@
 package com.example.coneccionbd.Modem
 
-class ManangerDb {
+import android.content.ContentValues
+import android.content.Context
+import android.database.sqlite.SQLiteDatabase
+
+data class ManangerDb(val context: Context) {
+
+    lateinit var bd:SQLiteDatabase
+
+    val  bdHelper = BdHelper(context)//llamado a mi conexion
+
+    //metodo para abrir bd en modo escritura
+    fun openBdWr(){
+
+        bd= bdHelper.writableDatabase
+    }
+    //abre base de datos modo lectura
+    fun openBdRd(){
+
+        bd=bdHelper.readableDatabase
+
+    }
+
+    fun  inserData():Long{
+
+        openBdWr() // abrir bd en modo escritura
+
+        //creo contenedor de valores para insertar data
+        val  contenedor =ContentValues()
+        contenedor.put("cod",1)
+        contenedor.put("nombre","cali")
+        contenedor.put("coddep",25)
+        //llamo metodo insert
+
+
+
+        val resul= bd.insert("ciudad",null,contenedor)
+            return resul
+
+    }
 }
